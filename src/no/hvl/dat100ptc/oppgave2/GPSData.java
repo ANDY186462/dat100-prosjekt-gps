b9c42def9149c61cfc0f3bbd1d489dde42852aeb
Alexrohjak <alexander.rohde.jakobsen@gmail.com>
2024-10-10 12:09:50 +0200

    Oppgave 2

--- src/no/hvl/dat100ptc/oppgave2/GPSData.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSData.java
@@ -9,40 +9,45 @@ public class GPSData {
 	protected int antall = 0;
 
 	public GPSData(int antall) {
-		
-		this.gpspoints = new GPSPoint[antall];	
-			this.antall=0;
-	
-}
+
+		this.gpspoints = new GPSPoint[antall];
+
+		this.antall = 0;
+	}
 
 	public GPSPoint[] getGPSPoints() {
 		return this.gpspoints;
 	}
-	
+
 	protected boolean insertGPS(GPSPoint gpspoint) {
 
-		boolean inserted = false;
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
-	
+		if (antall < gpspoints.length) {
+
+			gpspoints[antall] = gpspoint;
+
+			antall++;
+
+			return true;
+		}
+
+		return false;
 	}
 
 	public boolean insert(String time, String latitude, String longitude, String elevation) {
 
-		GPSPoint gpspoint;
+		GPSPoint gpspoint = GPSDataConverter.convert(time, latitude, longitude, elevation);
 
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
-		
+		return insertGPS(gpspoint);
 	}
 
 	public void print() {
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		
+		System.out.println("====== GPS Data - START ======");
+		
+		for (int i = 0; i < antall; i++) {
+			
+			System.out.println(gpspoints[i].toString());
+		}
+		System.out.println("====== GPS Data - SLUTT ======");
 	}
 }
