a037ea4face1ae4213f638fba37c3f0f82f5f734
Alexrohjak <alexander.rohde.jakobsen@gmail.com>
2024-10-15 13:24:38 +0200

    oppgave 2

--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -3,8 +3,7 @@ package no.hvl.dat100ptc.oppgave2;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class Main {
-
-	
+	 
 	public static void main(String[] args) {
 		GPSPoint gpsPoint = new GPSPoint(1,2.0,3.0,5);
 		
