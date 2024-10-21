40a26facda9c572c22a08532ee890f38d6be8b6f
Alexrohjak <alexander.rohde.jakobsen@gmail.com>
2024-10-21 12:40:11 +0200

    oppgave 5 a

--- src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
@@ -13,19 +13,19 @@ import javax.swing.JOptionPane;
 
 public class ShowProfile extends EasyGraphics {
 
-	private static final int MARGIN = 50;		// margin on the sides 
-	
+	private static final int MARGIN = 50; // margin on the sides
+
 	private static final int MAXBARHEIGHT = 500; // assume no height above 500 meters
-	
+
 	private GPSPoint[] gpspoints;
 
 	public ShowProfile() {
 
 		String filename = JOptionPane.showInputDialog("GPS data filnavn (uten .csv): ");
-		GPSComputer gpscomputer =  new GPSComputer(filename);
+		GPSComputer gpscomputer = new GPSComputer(filename);
 
 		gpspoints = gpscomputer.getGPSPoints();
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -39,18 +39,24 @@ public class ShowProfile extends EasyGraphics {
 		makeWindow("Height profile", 2 * MARGIN + 3 * N, 2 * MARGIN + MAXBARHEIGHT);
 
 		// top margin + height of drawing area
-		showHeightProfile(MARGIN + MAXBARHEIGHT); 
+		showHeightProfile(MARGIN + MAXBARHEIGHT);
 	}
 
 	public void showHeightProfile(int ybase) {
-		
-		int x = MARGIN; // f√∏rste h√∏yde skal tegnes ved MARGIN
-		int y;
-		
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
-		
 
+		int x = MARGIN; // startpunkt pÂ x-aksen
+
+		for (int i = 0; i < gpspoints.length; i++) {
+			
+			int height = (int) gpspoints[i].getElevation();
+
+			if (height < 0) { //dette s¯rger for at koden ignorerer negative h¯yder siden de er feil
+				height = 0; //og deretter om den er negativ setter vi den lik 0
+			}
+
+			drawLine(x, ybase, x, ybase - height);
+
+			x += 3; //dette legger litt mellomrom mellom ver linje i grafen
+		}
 	}
-
 }
