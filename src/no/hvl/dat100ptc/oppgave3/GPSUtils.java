ead5089ff9599aef560dc5a1c13af272f4774d76
Alexrohjak <alexander.rohde.jakobsen@gmail.com>
2024-10-22 11:06:17 +0200

    GPSUtils

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -1,4 +1,5 @@
 package no.hvl.dat100ptc.oppgave3;
+
 import java.util.*;
 
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
@@ -61,19 +62,17 @@ public class GPSUtils {
 
 	public static double distance(GPSPoint gpspoint1, GPSPoint gpspoint2) {
 
-
 		double latitude1 = gpspoint1.getLatitude();
 		double longitude1 = gpspoint1.getLongitude();
 		double latitude2 = gpspoint2.getLatitude();
 		double longitude2 = gpspoint2.getLongitude();
-		
+
 		double latitude1Rad = Math.toRadians(latitude1);
 		double latitude2Rad = Math.toRadians(latitude2);
 		double deltaPhi = Math.toRadians(latitude2 - latitude1);
 		double deltaLambda = Math.toRadians(longitude2 - longitude1);
-		
-	    double a = compute_a(latitude1Rad, latitude2Rad, deltaPhi, deltaLambda);
 
+		double a = compute_a(latitude1Rad, latitude2Rad, deltaPhi, deltaLambda);
 
 		double c = compute_c(a);
 
@@ -85,10 +84,9 @@ public class GPSUtils {
 //	for å regne ut verdien av a ved hjelp av formelen som er gitt i oppgava
 	private static double compute_a(double phi1, double phi2, double deltaphi, double deltadelta) {
 //haversine formel som er gitt i oppgava
-		double a = Math.pow(Math.sin(deltaphi / 2), 2) +
-	               Math.cos(phi1) * Math.cos(phi2) *
-	               Math.pow(Math.sin(deltadelta / 2), 2);
-		
+		double a = Math.pow(Math.sin(deltaphi / 2), 2)
+				+ Math.cos(phi1) * Math.cos(phi2) * Math.pow(Math.sin(deltadelta / 2), 2);
+
 		return a;
 
 	}
@@ -102,37 +100,33 @@ public class GPSUtils {
 
 //	Oppgave e
 	public static double speed(GPSPoint gpspoint1, GPSPoint gpspoint2) {
-		
-		int secs = gpspoint2.getTime() - gpspoint1.getTime();
-        double distance= distance(gpspoint1, gpspoint2);
-		double speed= distance / secs;
-		
-		return speed;
 
+		int secs = gpspoint2.getTime() - gpspoint1.getTime();
+		double distance = distance(gpspoint1, gpspoint2);
+		double speed = distance / secs;
+
+		return speed;
 
 	}
 
 //oppgave f ska retunere time,min, sek, i stregn metode
 	public static String formatTime(int secs) {
 
-		int timer= secs/3600;
-	    int min = (secs % 3600) / 60;
-	    int sek = secs % 60;
+		int timer = secs / 3600;
+		int min = (secs % 3600) / 60;
+		int sek = secs % 60;
 
-		
-	    String tidIstring = String.format("%02d:%02d:%02d", timer, min, sek);
+		String tidIstring = String.format("%02d:%02d:%02d", timer, min, sek);
 
-	    return String.format("%10s", tidIstring);
-
+		return String.format("%10s", tidIstring);
 
 	}
-
 
 //	oppgave g
 	public static String formatDouble(double d) {
 
-	    String str = String.format(Locale .US, "%10.2f", d);
-	    return str;
+		String str = String.format(Locale.US, "%10.2f", d);
+		return str;
 
 	}
 }
